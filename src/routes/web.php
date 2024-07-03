9902c85f3196ef26e27247f7ec24689f534c5c80
akko <meeeee0512@gmail.com>
2024-07-03 20:14:58 +0900

    反映

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,6 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TodoController;
+
+Route::get('/', [TodoController::class, 'index']);
+Route::post('/todos', [TodoController::class, 'store']);
 
 /*
 |--------------------------------------------------------------------------
